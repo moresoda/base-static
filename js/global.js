347c13ee61e1cdeda03a3733a0a2975f77ae4ade
Jon MacKinnon <jon@moresoda.co.uk>
2011-09-28 16:59:32 +0100

    Added js rule for font-face stylesheet to prevent duplication in web inspector

---
+++ js/global.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+
+	// Removes the font-face stylesheet once the google appended one is in to prevent duplication of styles in inspector
+	$('link[title=font-face]').remove();
+
+});
