e6c52b872cf1396189b48fdf4e29fcd9abea86c7
Jon MacKinnon <jon@moresoda.co.uk>
2012-07-02 14:24:47 +0100

    updated static to be in line with ee

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,6 +1,18 @@
+/*
+WebFontConfig = {
+	custom: {
+	    families: ['FONT-NAME'], // Font name as used in the stylesheet
+		urls: ['/assets/css/font-face.css']
+	},
+	active: function() {
+
+	}
+};
+(function() {var wf = document.createElement('script');wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +'://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';wf.type = 'text/javascript';wf.async = 'true';var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(wf, s);})();
+*/
 $(document).ready(function() {
 
-	// Removes the font-face stylesheet once the google appended one is in to prevent duplication of styles in inspector
-	$('link[title=font-face]').remove();
+
+
 
 });
